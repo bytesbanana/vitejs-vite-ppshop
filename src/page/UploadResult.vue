a46128e9cd64ae87747ba11500b65b00a4475782
bytesbanana <saharat.paynok@gmail.com>
2023-01-24 19:13:41 +0700

    add result page

--- src/page/UploadResult.vue
+++ src/page/UploadResult.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
-Result page
+        Result page
+        <button @click="liff.closeWindow">Close</button>
     </div>
 </template>
 
 <script setup>
+import liff from "@line/liff"
+import { onMounted, onBeforeMount } from "vue";
+
+
+onBeforeMount(async () => {
+
+    liff.ready.then(async () => {
+        if (liff.isInClient()) {
+            //Do something
+        }
+    });
+    await liff.init({
+        liffId: '1657838953-QPY6AODk'
+    })
+})
+
 
 </script>
 
